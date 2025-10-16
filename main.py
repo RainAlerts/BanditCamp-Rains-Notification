23bf913590748bfa24808d9f52d8139ed0e39bee
Rain Alert Hub <di9wksjs@gmail.com>
2025-10-16 23:58:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+a = '''
+Наш бот со всеми ссылками на каналы с алертами о рейнах - @rainsalertbot
+
+Новости проекта - @rainshub
+
+Дискорд с тем же ботом - discord (https://discord.gg/N4VFJ65Dvq)
+'''
+print(a)
+input()
